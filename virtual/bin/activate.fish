f98a6f672b2d1c8e887bb8285b8256b3bb3b2863
JeromeMberia <jeromemberia@gmail.com>
2020-05-08 15:42:27 +0300

    redoing virtual environment

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/jerome/the._pitch/virtual"
+set -gx VIRTUAL_ENV "/home/jerome/Desktop/the._pitch/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
